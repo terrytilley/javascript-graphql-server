8f7f467c63700e0275e03537577e6a10f98c7d71
Terry Tilley <terry@terrytilley.com>
2018-10-06 16:32:41 +0100

    :tada: init commit :ring:

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'no-console': 0,
+    camelcase: [
+      'error',
+      {
+        properties: 'never',
+        ignoreDestructuring: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: true,
+        bracketSpacing: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# javascript-graphql-server

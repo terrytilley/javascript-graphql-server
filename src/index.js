7109a404cad6bb203484fb63406bf815173bbfa1
Terry Tilley <terry@terrytilley.com>
2018-10-06 17:20:07 +0100

    merge graphql schemas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import 'dotenv/config';
+import path from 'path';
 import cors from 'cors';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import models from './models';
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+const resolvers = mergeResolvers(
+  fileLoader(path.join(__dirname, './resolvers'))
+);
 
 const app = express();
 const port = process.env.PORT || 8000;

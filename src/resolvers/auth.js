80e6f884e801b5e053f97587cc49d6b6d90beed5
Terry Tilley <terry@terrytilley.com>
2018-10-14 20:11:34 +0100

    moved forgotPassword into auth

--- src/resolvers/auth.js
+++ src/resolvers/auth.js
@@ -1,7 +1,10 @@
 import bcrypt from 'bcrypt';
 import { AuthenticationError } from 'apollo-server-express';
+import userValidation, { passwordValidation } from '../validation/user';
+import createForgotPasswordLink from '../utils/createForgotPasswordLink';
+import forgotPasswordLockAccount from '../utils/forgotPasswordLockAccount';
 import removeAllUserSessions from '../utils/removeAllUserSessions';
-import userValidation from '../validation/user';
+import { forgotPasswordPrefix } from '../constants';
 
 export default {
   Mutation: {
@@ -47,6 +50,38 @@ export default {
       }
 
       return false;
+    },
+    forgotPassword: async (_, { email }, { models, redis }) => {
+      const user = await models.User.findOne({ where: { email } });
+      if (!user) return false;
+
+      await forgotPasswordLockAccount(user.id, redis);
+      await createForgotPasswordLink('', user.id, redis);
+      // @todo: Send email with url
+
+      return true;
+    },
+    resetPassword: async (_, { newPassword, key }, { models, redis }) => {
+      const redisKey = `${forgotPasswordPrefix}${key}`;
+      const userId = await redis.get(redisKey);
+
+      if (!userId) throw new Error('Key has expired');
+
+      try {
+        await passwordValidation.validate({ password: newPassword });
+      } catch (err) {
+        return err;
+      }
+
+      const hashedPassword = await bcrypt.hash(newPassword, 12);
+      const deleteKeyPromise = redis.del(redisKey);
+      const updatePromise = models.User.update(
+        { password: hashedPassword, locked: false },
+        { where: { id: userId } }
+      );
+
+      await Promise.all([updatePromise, deleteKeyPromise]);
+      return true;
     },
   },
 };
